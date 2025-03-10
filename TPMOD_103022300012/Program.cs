357bbacc271ef817447b549e23ade117e066ca4c
Zipooxy <142865402+Zipooxy@users.noreply.github.com>
2025-03-10 23:12:22 +0700

    Add project files.

---
+++ TPMOD_103022300012/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class KodePos
+{
+    private static readonly Dictionary<string, string> kodePosMap = new Dictionary<string, string>
+    {
+        { "Batununggal", "40266" },
+        { "Kujangsari", "40287" },
+        { "Mengger", "40267" },
+        { "Wates", "40256" },
+        { "Cijaura", "40287" },
+        { "Jatisari", "40286" },
+        { "Margasari", "40286" },
+        { "Sekejati", "40286" },
+        { "Kebonwaru", "40272" },
+        { "Maleer", "40274" },
+        { "Samoja", "40273" }
+    };
+
